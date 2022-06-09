ef16b1551f2a410d3df386ddd86335cd958e0963
Cedric Kulbach <42866082+kulbachcedric@users.noreply.github.com>
2022-06-09 15:08:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ base_packages = [
     "msgpack==1.0.2",
     "networkx==2.5.1",
     "numba==0.53.1",
-    "numpy==1.22.4",
     "oauthlib==3.1.1",
     "opt-einsum==3.3.0",
     "packaging==21.0",
