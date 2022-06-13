ec9e63a16a9e50645b22962a84874e301cafc43c
Jacqueline Hoellig <hoellig@fzi.de>
2022-06-13 09:31:41 +0200

    fix in setup.py

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ base_packages = [
     "pytz==2021.3",
     "scikit-learn==0.24.2",
     "shap==0.39.0",
-    "geos",
-    "Shapely==1.7.1",
     "sktime==0.8.0",
     "tensorflow==2.6.0",
     "keras==2.6",
