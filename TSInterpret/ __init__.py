2b226d5ced51b59ccb16357195342027f7efcc49
Jacqueline Hoellig <hoellig@fzi.de>
2022-06-15 11:36:45 +0200

    Delected Dependency on data script

--- TSInterpret/ __init__.py
+++ TSInterpret/ __init__.py
@@ -13,7 +13,6 @@ from .__version__ import __version__  # noqa: F401
 
 __all__ = [
     "ClassificationModels",
-    "data",
     "Evaluate",
     "InterpretabiliyModels",
     "Models",
--- TSInterpret/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import load_data
-__all__ = [
-    "load_data"
-]
