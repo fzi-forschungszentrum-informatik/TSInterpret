bed84fa3b6855f267a2a3ecc7f79df156a957eb2
Jacqueline Hoellig <hoellig@fzi.de>
2022-07-11 17:12:37 +0200

    setip.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import io
 import os
 import subprocess
@@ -19,7 +18,7 @@ LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 URL = "https://ipe-wim-gitlab.fzi.de/hoellig/interpretabilitytimeseries"
 EMAIL = "hoellig@fzi.de"
 AUTHOR = "Jacqueline Hoellig"
-REQUIRES_PYTHON = ">=3.6.0"
+REQUIRES_PYTHON = ">=3.60"
 
 # Package requirements.
 base_packages = [
@@ -34,15 +33,12 @@ base_packages = [
     "pytz==2021.3",
     "scikit-learn==0.24.2",
     "shap==0.39.0",
-    "Shapely==1.7.1",
-    "sktime==0.8.0",
-    "tensorflow==2.6.0",
-    "keras==2.6",
     "tqdm==4.62.3",
     "tsfresh==0.18.0",
     "wildboar==1.0.10",
+    "tensorflow",
+    "keras",
     "tslearn",
-    "torch",
     "seaborn",
     "scikit_optimize",
     "mlrose",
@@ -52,6 +48,15 @@ base_packages = [
     "captum",
     "pyts",
     "deprecated"
+    
+]
+
+torch_packages = base_packages + [
+    "torch",
+]
+
+tensorflow_packages = base_packages + [
+    "tensorflow"
 ]
 
 dev_packages = base_packages + [
@@ -64,13 +69,17 @@ dev_packages = base_packages + [
 docs_packages = [
     "flask==2.0.2",
     "ipykernel==6.9.0",
+    "numpy==1.21.6",
     "mike==0.5.3",
     "mkdocs==1.2.3",
     "mkdocs-awesome-pages-plugin==2.7.0",
     "mkdocs-material==8.1.11",
     "mkdocstrings==0.18.0",
+    "mkdocs-material-extensions",
+    "mkdocs-autorefs",
     "ipython_genutils==0.1.0",
     "mkdocs-jupyter==0.20.0",
+    "mkdocs-bibtex==2.8.1",
     "nbconvert==6.4.2",
     "numpydoc==1.2",
     "spacy==3.2.2",
@@ -105,8 +114,10 @@ setuptools.setup(
         "dev": dev_packages,
         "test": dev_packages,
         "docs": docs_packages,
+        "torch": torch_packages,
+        "tensorflow" : tensorflow_packages,
         "all": dev_packages,# + docs_packages,
-        ":python_version == '3.6'": ["dataclasses"],
+        ":python_version == '3.7'": ["dataclasses"],
     },
     include_package_data=True,
     license="BSD-3",
