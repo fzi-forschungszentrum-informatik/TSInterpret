9b620e0f54c156b5ab44bd9f108ed6a6dce043e3
kulbach <kulbach@fzi.de>
2022-07-11 10:52:07 +0200

    Some changes

---
+++ TSInterpret/__init__.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from . import (
+  ClassificationModels,
+  data,
+  Evaluate,
+  InterpretabiliyModels,
+  Models,
+  constants
+
+
+)
+from .__version__ import __version__  # noqa: F401
+
+__all__ = [
+    "ClassificationModels",
+    "Evaluate",
+    "InterpretabiliyModels",
+    "Models",
+    "constants"
+]
